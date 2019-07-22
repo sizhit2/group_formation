Fixed bug with average gpa being calculated twice

Python_Version/GGA.py
@@ -6,7 +6,7 @@ from GeneticAlgorithmFunctions import GeneticAlgorithmFunctions
 import copy
 import time
 
-def run_ga(dir='../data/Run10_noGPAweight/'):
+def run_ga(dir='../data/'):
     in_data = parse_input_data(dir+'input_data.txt')
 
     # For the design variable matrix
@@ -35,12 +35,12 @@ def run_ga(dir='../data/Run10_noGPAweight/'):
     print ("Reading Excel data....")
     student_list = read_from_csv(dir+data_filename, num_projects)
 
-    # To fake GPA stuff
-    gpa_list = np.random.normal(2.5, 0.1, num_students)
-    for (i, stud) in enumerate(student_list):
-        stud.gpa = gpa_list[i]
-        if stud.selected_partner:
-            stud.partner_gpa = gpa_list[i]
+    # To fake GPA stuff - comment out when using real data
+    # gpa_list = np.random.normal(2.5, 0.1, num_students)
+    # for (i, stud) in enumerate(student_list):
+    #     stud.gpa = gpa_list[i]
+    #     if stud.selected_partner:
+    #         stud.partner_gpa = gpa_list[i]
 
     student_pref_matrix = np.zeros((num_students, num_projects))
     max_satisfaction = 0
@@ -179,7 +179,7 @@ def run_ga(dir='../data/Run10_noGPAweight/'):
 
 # Temporary to run on new data
 def t_main():
-    run_ga()
+    run_ga(dir='../data/RunX_debug/')
 
 def main():
     num_iter = 5

Python_Version/OptimizationFunctions.py
@@ -153,6 +153,7 @@ class OptimizationFunctions(object):
             sigma = sigma / individual.num_projects
 
         # Cost associated with average GPA per project
+        individual.avg_gpa_per_project = np.zeros(individual.num_projects)
         sigma_gpa = 0.0
         if abs(gamma_gpa) > 0.0:
             fictitious_num_students = np.zeros(individual.num_projects)
@@ -167,15 +168,15 @@ class OptimizationFunctions(object):
                 fictitious_num_students[proj] += 1
                 individual.avg_gpa_per_project[proj] += pair_gpa
 
-            # print (individual.avg_gpa_per_project)
-            # print (fictitious_num_students)
             # Getting variance of average GPA per project
             for proj in range(individual.num_projects):
                 if fictitious_num_students[proj] != 0:
                     individual.avg_gpa_per_project[proj] /= fictitious_num_students[proj]
+
                     sigma_gpa += (individual.avg_gpa_per_project[proj] - class_avg_gpa) ** 2
+
             sigma_gpa /= individual.num_projects
-        # print (sigma_gpa)
+
         # Combine the 3 costs to get the final fitness
         individual.fitness = satisfaction_score
         if sigma > 0: